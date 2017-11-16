a883a37c77d0a8fffc1a53d2739853847beed449
risa2 <blazek.risa@gmail.com>
2017-11-16 19:12:44 +0100

    Vector

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 extern crate sdl2;
 
 use std::thread;
+use std::f32;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::event;
 
-struct Scene {
-    width: i32,
-    height: i32
-}
 
 struct Block {
     color: Color,
@@ -16,19 +13,25 @@ struct Block {
     y: i32
 }
 
+type Blocks=Vec<Block>;
+
+struct Scene {
+    width: i32,
+    height: i32,
+    blocks: Blocks
+}
+
 struct Ball<'a> {
-    x: i32,
-    y: i32,
-    x_move: i32,
-    y_move: i32,
+    x: f32,
+    y: f32,
+    direction: f32,
+    speed: f32,
     scene: &'a Scene
 }
 
 struct Palka {
     x: i32
 }
-
-type Blocks=Vec<Block>;
 
 
 fn make_blocks(left: u32, top: u32, width: u32, height: u32, x_count: u32, y_count: u32)->Blocks {
@@ -45,36 +48,62 @@ fn make_blocks(left: u32, top: u32, width: u32, height: u32, x_count: u32, y_cou
 impl Block {
     const WIDTH:u32=20;
     const HEIGHT:u32=10;
+    fn to_rect(&self)->Rect {
+        Rect::new(self.x, self.y, Block::WIDTH, Block::HEIGHT)
+    }
     fn render(&self, renderer: &mut sdl2::render::WindowCanvas) {
         renderer.set_draw_color(self.color);
         renderer.fill_rect(Rect::new(self.x, self.y, Block::WIDTH, Block::HEIGHT));
     }
 }
 
+impl Scene {
+    fn render(&self, renderer: &mut sdl2::render::WindowCanvas) {
+        renderer.set_draw_color(Color::RGB(0,200,0));
+        renderer.clear();
+
+        let blocks=&self.blocks;
+        for block in blocks {
+            block.render(renderer);
+        }
+    }
+}
+
 impl<'a> Ball<'a> {
-    const SIZE: i32=20;
+    const SIZE: f32=20.0;
+    fn to_rect(&self)->Rect {
+        Rect::new(self.x, self.y, Ball::SIZE as u32, Ball::SIZE as u32)
+    }
     fn render(&self, renderer: &mut sdl2::render::WindowCanvas) {
         renderer.set_draw_color(Color::RGB(0,0,255));
-        renderer.fill_rect(Rect::new(self.x-Ball::SIZE/2, self.y-Ball::SIZE/2, Ball::SIZE as u32, Ball::SIZE as u32));
+        renderer.fill_rect(self.to_rect());
 
     }
 
     fn update(&mut self) {
-        self.x+=self.x_move;
-        self.y+=self.y_move;
+        self.x+=self.speed*self.direction.cos();
+        self.y+=self.speed*self.direction.sin();
 
-        if self.x>self.scene.width-Ball::SIZE/2 || self.x<Ball::SIZE/2 {
-            self.x_move=-self.x_move;
+        if self.x>self.scene.width as f32-Ball::SIZE || self.x<0.0 {
+            self.direction=std::f32::consts::PI-self.direction;
         }
 
-        if self.y>self.scene.height-Ball::SIZE/2 || self.y<Ball::SIZE/2 {
-            self.y_move=-self.y_move;
+        if self.y>self.scene.height as f32-Ball::SIZE || self.y<0.0 {
+            self.direction=-self.direction;
+        }
+        if self.direction<0.0 {
+            self.direction+=2.0*std::f32::consts::PI;
+        }
+
+        for block in self.scene.blocks {
+            if self.to_rect().has_intersection(block.to_rect()) {
+
+            }
         }
     }
 }
 
 struct App<'a> {
-    blocks: Blocks,
     ball: Ball<'a>,
     palka: Palka,
     scene: &'a Scene
@@ -83,13 +112,7 @@ struct App<'a> {
 impl<'a> App<'a> {
     fn render(&mut self, renderer: &mut sdl2::render::WindowCanvas) {
 
-        renderer.set_draw_color(Color::RGB(0,200,0));
-        renderer.clear();
-
-        let blocks=&self.blocks;
-        for block in blocks {
-            block.render(renderer);
-        }
+        scene.render(renderer);
         self.ball.render(renderer);
         renderer.present();
     }
@@ -108,7 +131,7 @@ fn main() {
     let scene=Scene{width: 1000, height: 600};
     let mut app=App {
         blocks: make_blocks(10, 0, 980, 600, 10, 10),
-        ball: Ball{x: 300, y: 400, x_move: 2, y_move: -1, scene: &scene},
+        ball: Ball{x: 300.0, y: 400.0, direction: 3.0, speed: 2.0, scene: &scene},
         palka: Palka{x: 300},
         scene: &scene
     };
